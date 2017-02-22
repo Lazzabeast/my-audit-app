51b6de66baeb6e2271ef26c1eb3a762221b07231
Larry Allen <laurence.allen546@gmail.com>
2017-02-22 16:11:55 +0000

    Modified static url with home and static

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,6 +119,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/Lazzabeast/my-audit-app/'
+STATIC_URL = '/home/Lazzabeast/my-audit-app/static'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
